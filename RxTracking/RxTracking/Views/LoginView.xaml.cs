78784588f393d3d004841b31983ae38c35d45dc4
Matthew Brown <barakis@barakisbrown.com>
2016-03-20 18:29:23 -0500

    Add notification to open ContactView and OrderView

--- RxTracking/RxTracking/Views/LoginView.xaml.cs
+++ RxTracking/RxTracking/Views/LoginView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using GalaSoft.MvvmLight.Messaging;
 
 namespace RxTracking.Views
 {
@@ -14,6 +15,23 @@ namespace RxTracking.Views
         public LoginView()
         {
             InitializeComponent();
+            Messenger.Default.Register<NotificationMessage>(this, NotificationMessageReceived);
+        }
+
+        private void NotificationMessageReceived(NotificationMessage obj)
+        {
+            if (obj.Notification == "Open ContactView")
+            {
+                Messenger.Default.Unregister<NotificationMessage>("Open ContactView");
+                var contact = new ContactView();                
+                contact.ShowDialog();
+            }
+            else if (obj.Notification == "Open OrderView")
+            {
+                Messenger.Default.Unregister<NotificationMessage>("Open OrderView");
+                var orders = new OrderView();
+                orders.Show();
+            }
         }
 
         private void passwBox_PasswordChanged(object sender, RoutedEventArgs e)
