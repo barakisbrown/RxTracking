5d20efeaebd231e5454aafc5cc0ca6176496af91
Matthew Brown <barakis@barakisbrown.com>
2016-03-20 18:27:46 -0500

    Order View

---
+++ RxTracking/RxTracking/Views/OrderView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace RxTracking.Views
+{
+    /// <summary>
+    /// Description for OrderView.
+    /// </summary>
+    public partial class OrderView : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the OrderView class.
+        /// </summary>
+        public OrderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
