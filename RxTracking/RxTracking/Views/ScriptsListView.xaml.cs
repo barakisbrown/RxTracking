faf166298b20a4fdac2579e97af1c0b78612db4f
Matthew Brown <barakis@barakisbrown.com>
2017-02-08 08:25:20 -0600

    Added ScriptListView and ViewModel which will replace Orders and OrderViewModel

---
+++ RxTracking/RxTracking/Views/ScriptsListView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace RxTracking.Views
+{
+    /// <summary>
+    /// Description for ScriptsListView.
+    /// </summary>
+    public partial class ScriptsListView : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the ScriptsListView class.
+        /// </summary>
+        public ScriptsListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
