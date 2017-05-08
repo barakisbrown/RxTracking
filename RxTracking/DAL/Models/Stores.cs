5f64418b29cb9aabc432a172011a219502328c54
Matthew Brown <lokislayer@gmail.com>
2017-05-08 10:28:35 -0500

    Stores Purchases Model

---
+++ RxTracking/DAL/Models/Stores.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GalaSoft.MvvmLight;
+
+namespace DAL.Models
+{
+    /// <summary>
+    /// Table of all the stores where prescriptions where purchased
+    /// </summary>
+    public class Stores : ObservableObject
+    {
+        // primary key
+        private int _id;
+        // Name of the drug store
+        private string _storeName;
+
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        [Key]
+        public int Id
+        {
+            get { return _id;}
+            set { Set(ref _id, value); }
+        }
+
+        /// <summary>
+        /// Name of the Drug Store
+        /// </summary>
+        [Required]
+        public string Name
+        {
+            get { return _storeName;}
+            set { Set(ref _storeName, value); }
+        }
+    }
+}
