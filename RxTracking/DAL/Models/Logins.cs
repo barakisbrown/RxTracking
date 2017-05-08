859257dbb5e2f9706cc884c974035c28191816e7
Matthew Brown <lokislayer@gmail.com>
2017-05-08 10:28:21 -0500

    UserLogin Model

---
+++ RxTracking/DAL/Models/Logins.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GalaSoft.MvvmLight;
+
+namespace DAL.Models
+{
+    public class Logins : ObservableObject
+    {
+        private int _id;
+        private string _name;
+        private byte[] _password;
+
+        /// <summary>
+        /// Primary Key
+        /// </summary>
+        [Key]
+        public int Id
+        {
+            get { return _id;;}
+            set { Set(ref _id, value); }
+        }
+
+
+        /// <summary>
+        /// Username
+        /// </summary>
+        [Required]
+        public string Name
+        {
+            get { return _name;}
+            set { Set(ref _name, value); }
+        }
+
+        /// <summary>
+        /// Hashvalue of the password
+        /// </summary>
+        [Required]
+        public byte[] Password
+        {
+            get { return _password;}
+            set { Set(ref _password, value); }
+        }
+    }
+}
