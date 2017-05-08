0fd16b983ae6e8cc38bee3538ba4c4ee8704e5f3
Matthew Brown <lokislayer@gmail.com>
2017-05-08 09:25:39 -0500

    DbContext
    This is my Context for the RxTracking Application

---
+++ RxTracking/DAL/Context/DbContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+using MySql.Data.Entity;
+
+namespace DAL.Context
+{
+    /// <summary>
+    /// My Context for the DAL part of RxTracking
+    /// </summary>
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class DbContext : System.Data.Entity.DbContext
+    {
+        /// <summary>
+        /// Created a connection 
+        /// </summary>
+        public DbContext() : base("ConnectionString")
+        {
+            
+        }
+
+        /// <summary>
+        /// Using an existing connection and created a DbContext from it or simply returns it.
+        /// </summary>
+        /// <param name="existingConnection"></param>
+        /// <param name="contextOwnConnection"></param>
+        public DbContext(DbConnection existingConnection, bool contextOwnConnection)
+            : base(existingConnection, contextOwnConnection)
+        {
+            
+        }
+
+        /// <summary>
+        /// Used when the model is first build and then created/stored to the backend
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
