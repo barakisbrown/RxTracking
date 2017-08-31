5d1343c39ea332da64616ce44c88361858697f44
Matthew Brown <lokislayer@gmail.com>
2017-08-31 10:30:09 -0500

    Change property to use Expression Bodies

--- RxTracking/DAL/Models/UserInfo.cs
+++ RxTracking/DAL/Models/UserInfo.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections.Generic;
-using GalaSoft.MvvmLight;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-
-namespace DAL.Models
+﻿namespace DAL.Models
 {
+    using GalaSoft.MvvmLight;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// UserInfo Object to Table Representation
+    /// </summary>
     public class UserInfo : ObservableObject
     {
         private int _id;
@@ -34,22 +36,22 @@ namespace DAL.Models
         [Key]
         public int Id
         {
-            get { return _id; }
-            set { Set(ref _id, value); }
+            get => _id;
+            set => Set(ref _id, value);
         }
 
         [Required]
         public string FirstName
         {
-            get {return _firstName;}
-            set { Set(ref _firstName, value); }
+            get => _firstName;
+            set => Set(ref _firstName, value);
         }
 
         [Required]
         public string LastName
         {
-            get { return _lastName; }
-            set { Set(ref _lastName, value); }
+            get => _lastName;
+            set => Set(ref _lastName, value);
         }
 
         [NotMapped]
@@ -58,43 +60,43 @@ namespace DAL.Models
         [Required]
         public string Address
         {
-            get { return _address; }
-            set { Set(ref _address, value); }
+            get => _address;
+            set => Set(ref _address, value);
         }
 
         [Required]
         public string City
         {
-            get { return _city; }
-            set { Set(ref _city, value); }
+            get => _city;
+            set => Set(ref _city, value);
         }
 
         [Required, MaxLength(2)]
         public char[] State
         {
-            get { return _state; }
-            set { Set(ref _state, value); }
+            get => _state;
+            set => Set(ref _state, value);
         }
         
         [Required, MaxLength(10)]
         public char[] ZipCode
         {
-            get { return _zipCode;}
-            set { Set(ref _zipCode, value); }
+            get => _zipCode;
+            set => Set(ref _zipCode, value);
         }
 
         [Required, MaxLength(13)]
         public char[] PhoneNumber
         {
-            get { return _phoneNumber;}
-            set { Set(ref _phoneNumber, value); }
+            get => _phoneNumber;
+            set => Set(ref _phoneNumber, value);
         }
         
         [Required]
         public string Email
         {
-            get { return _email; }
-            set { Set(ref _email, value); }
+            get => _email;
+            set => Set(ref _email, value);
         }  
         
         /// <summary>
