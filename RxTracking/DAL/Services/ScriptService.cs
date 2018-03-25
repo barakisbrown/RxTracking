1133bc50e078e93dcd16c0a92308421519b330d0
Matthew Brown <lokislayer@gmail.com>
2018-03-25 03:28:07 -0500

    SciptService DataAccess Class
    This class will allow me to acces the Script table snd get data from it.

---
+++ RxTracking/DAL/Services/ScriptService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using DAL.Models;
+
+namespace DAL.Services
+{
+    public class ScriptService : IDataService<Script>
+    {
+        private static string connectionString = "server=lokislayer.com;database=rxstore;uid=BARAKIS;password=BarakisMJB48;persistsecurityinfo=True;";
+
+        public void Add(Script newValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Script Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<Script> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Script t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
