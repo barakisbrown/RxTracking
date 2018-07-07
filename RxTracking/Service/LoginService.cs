3d6c9fce76fd720571002c4faa2db05734b7344d
Matthew Brown <lokislayer@gmail.com>
2018-07-07 08:08:06 -0500

    Added LoginService Class

---
+++ RxTracking/Service/LoginService.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using DAL.Models;
+using System;
+
+namespace Service
+{
+    public class LoginService : ILogin
+    {
+        public bool CheckCredentials(string userName, string userPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckUser(string usrName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserInfo FetchUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
