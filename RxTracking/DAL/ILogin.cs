79b5fe7436efd30bba197b7ca6d645c631779626
Matthew Brown <lokislayer@gmail.com>
2018-07-07 08:07:46 -0500

    Added ILogin Interface

---
+++ RxTracking/DAL/ILogin.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Models;
+
+namespace DAL
+{
+    /// <summary>
+    /// Interface for my Login Object. I am using it to check the following:
+    /// A. Given a username, does it exist
+    /// B. Given a usename and password, does it exist
+    /// C. Fetch the Userinfo Info based on those valid credentials
+    /// </summary>
+    public interface ILogin
+    {
+        bool CheckUser(string usrName);
+        bool CheckCredentials(string userName, string userPassword);
+        UserInfo FetchUser(string userName);
+    }
+}
